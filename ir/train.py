9b07b5d837189370ec36eb2c5210957c9664e954
janglucky <janglucky@163.com>
2024-08-21 06:46:34 +0000

    first commit

--- ir/train.py
+++ ir/train.py
@@ -4,9 +4,9 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 import os.path as osp
 from basicsr.train import train_pipeline
 
-import isr.archs
-import isr.data
-import isr.models
+import ir.archs
+import ir.data
+import ir.models
 
 if __name__ == '__main__':
     root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
